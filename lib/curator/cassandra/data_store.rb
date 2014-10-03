8c2ee129d88c36a1eff9840261404888cd1a0bbf
Braintree <code@getbraintree.com>
2014-10-03 23:39:03 +0000

    Get save working.

--- lib/curator/cassandra/data_store.rb
+++ lib/curator/cassandra/data_store.rb
@@ -10,7 +10,6 @@ module Curator
 
         yml_config = YAML.load(File.read(Curator.config.cassandra_config_file))[Curator.config.environment]
         @client = ::Cassandra.connect(yml_config)
-        @keyspace_with_environment = Curator.config.keyspace + "_" + Curator.config.environment
         @client
       end
 
@@ -23,22 +22,41 @@ module Curator
       end
 
       def save(options)
-        columns = options[:value].keys.map(&:to_s)
+        _create_table_if_needed(options)
+
+        columns = options[:value].keys.map(&:to_s).join(",")
         question_marks = options[:value].size.times.map { "?" }.join(",")
-        statement = _session.prepare("INSERT INTO #{options[:collection_name]} (#{columns}) VALUES (#{question_marks})")
-        _session.execute(statement, *options[:value].values)
+        statement = _session.prepare("INSERT INTO #{options[:collection_name]} (key, #{columns}) VALUES (?, #{question_marks})")
+        _session.execute(statement, *options[:value].values.unshift(options[:key]))
       end
 
       def reset!
-        keyspace = client.keyspace(_keyspace_with_environment)
-        if keyspace
-          keyspace.each_table do |table|
-            client.execute("TRUNCATE #{table.name}")
+        if _keyspace
+          _keyspace.each_table do |table|
+            _session.execute("DROP TABLE #{table.name}")
           end
         end
       end
 
-      def _keyspace_with_environment
+      def _create_table_if_needed(options)
+        table_name = options[:collection_name]
+        return if _keyspace && _keyspace.tables.map(&:name).include?(table_name)
+
+        columns = options[:value].keys.map { |key| "#{key} #{_cql_type(options[:value][key])}" }.join(",")
+
+        _session.execute(<<-CQL)
+          CREATE TABLE #{table_name} (
+            key text PRIMARY KEY,
+            #{columns}
+          )
+        CQL
+      end
+
+      def _keyspace
+        @keyspace ||= client.keyspace(_keyspace_name)
+      end
+
+      def _keyspace_name
         Curator.config.keyspace + "_" + Curator.config.environment
       end
 
@@ -47,11 +65,19 @@ module Curator
 
         @session = client.connect
         @session.execute(<<-END)
-        CREATE KEYSPACE IF NOT EXISTS #{_keyspace_with_environment} WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':3};
+        CREATE KEYSPACE IF NOT EXISTS #{_keyspace_name} WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':3};
         END
-        @session.execute("USE #{_keyspace_with_environment}")
+        @session.execute("USE #{_keyspace_name}")
         @session
       end
+
+      def _cql_type(obj)
+        case obj
+        when Integer then "bigint"
+        else "text"
+        end
+      end
+
     end
   end
 end
